a6ae54dd4df7fa9179bd500e8c41948d63fa7727
Georgios Giannopoulos <georgegiannopoulos2@gmail.com>
2025-07-03 19:55:55 +0300

    Add TimestampUtil documentation

--- src/main/java/com/github/giorgosg1an/TimestampUtil.java
+++ src/main/java/com/github/giorgosg1an/TimestampUtil.java
@@ -3,7 +3,19 @@ package com.github.giorgosg1an;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Utility class for generating formatted timestamp strings.
+ * 
+ * @author Giannopoulos Georgios
+ */
 public class TimestampUtil {
+    /**
+     * Returns the current date and time as a formatted string.
+     * The format used is "yyyy-MM-dd HH:mm:ss.SSS", which includes the year, month, day,
+     * hour, minute, second, and milliseconds.
+     *
+     * @return the current timestamp as a formatted string
+     */
     public static String now() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
